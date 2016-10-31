6fabae77bd1215a0fe0164a5d66052c2f907d59f
irislee <irislee.music@gmail.com>
2016-10-31 01:06:04 -0400

    added scrollto

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
 $('#menu').click(function() {
   $(this).toggleClass('expanded').siblings('div').slideToggle(0);
 });
+
+$('#mobile-nav a').click(function() {
+  $('#menu').toggleClass('expanded').siblings('div').slideToggle(0);
+});
+
+$(document).ready(function(){
+  $('mobile-nav a').click(function(){
+    var href = $(this).attr('href');
+    $(window).scrollTo(href, 100, {offset: -500});
+  });
+
+});
