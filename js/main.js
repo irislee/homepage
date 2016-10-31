6bba818527318b13b6e927d6612526fcd3deafb7
irislee <irislee.music@gmail.com>
2016-10-31 10:41:46 -0400

    tidied css

--- js/main.js
+++ js/main.js
@@ -6,10 +6,10 @@ $('#mobile-nav a').click(function() {
   $('#menu').toggleClass('expanded').siblings('div').slideToggle(0);
 });
 
+
 $(document).ready(function(){
   $('mobile-nav a').click(function(){
     var href = $(this).attr('href');
     $(window).scrollTo(href, 100, {offset: -500});
   });
-
 });
